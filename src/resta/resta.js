13cc807cd8a772736c598b45d9557d597fc292d8
DanielOspina <danielospinaromam@gmail.com>
2023-11-21 00:25:42 -0500

    Probando subir archivo

--- src/resta/resta.js
+++ src/resta/resta.js
@@ -2,7 +2,7 @@ function rest(num1, num2) {
     if (typeof num1 != "number" || typeof num2 != "number") {
       return "datos invalidos";
     }
-    return num1 - num2;
+    return num1-num2;
   }
   
   module.exports = rest;
